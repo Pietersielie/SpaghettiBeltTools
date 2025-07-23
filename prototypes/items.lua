e96790ad9d54c6779c9ba1bb25bb94dfe643ed17
Pietersielie <pietersieliepcp@gmail.com>
2025-07-23 15:37:39 +0200

    Add incorrect functionality to remove a belt thread. Add new icon.
    Redo documentation to match LuaDoc format.
    Rename mod to better reflect functionality.

--- prototypes/items.lua
+++ prototypes/items.lua
@@ -1,20 +1,21 @@
 local beltThreadUpgradeTool = table.deepcopy(data.raw["selection-tool"]["selection-tool"])
+local beltThreadRemoveTool = table.deepcopy(data.raw["selection-tool"]["selection-tool"])
 local selectionTypeFilter = {"splitter", "transport-belt", "underground-belt", "loader", "loader-1x1"}
 
 if (mods['lane-splitters'] or mods['lane-balancers']) then
     table.insert(selectionTypeFilter, "lane-splitter")
 end
 
-beltThreadUpgradeTool.name = "beltThreadUpgrader-selection-tool"
+beltThreadUpgradeTool.name = "beltThreadUpgrader-upgrade-tool"
 beltThreadUpgradeTool.flags = {"only-in-cursor", "spawnable", "not-stackable"}
 beltThreadUpgradeTool.subgroup = "tool"
-beltThreadUpgradeTool.order = "c[automated-construction]-d[beltThreadUpgrader-selection-tool]"
-beltThreadUpgradeTool.icon = "__BeltThreadUpgrades__/graphics/icons/beltThreadUpgrader-shortcut-x60.png"
+beltThreadUpgradeTool.order = "c[automated-construction]-d[beltThreadUpgrader-upgrade-tool]"
+beltThreadUpgradeTool.icon = "__BeltThreadUpgrades__/graphics/icons/beltThreadUpgrader-upgrade-tool-icon-x60.png"
 beltThreadUpgradeTool.icon_size = 60
 
 -- Default selection (Upgrade only connected belts of same tier)
 beltThreadUpgradeTool.select.cursor_box_type = "entity"
-beltThreadUpgradeTool.select.mode = {"upgrade"}
+beltThreadUpgradeTool.select.mode = "upgrade"
 beltThreadUpgradeTool.select.entity_filter_mode = "whitelist"
 beltThreadUpgradeTool.select.entity_type_filters = selectionTypeFilter
 beltThreadUpgradeTool.select.border_color = {0, 0.8, 0}
@@ -35,5 +36,28 @@ beltThreadUpgradeTool.reverse_select.border_color = {1, 0, 0}
 beltThreadUpgradeTool.alt_reverse_select = table.deepcopy(beltThreadUpgradeTool.reverse_select)
 beltThreadUpgradeTool.alt_reverse_select.border_color = {0.8, 0, 0.8}
 
+-- Base for removal tool
+beltThreadRemoveTool.name = "beltThreadUpgrader-remove-tool"
+beltThreadRemoveTool.flags = {"only-in-cursor", "spawnable", "not-stackable"}
+beltThreadRemoveTool.subgroup = "tool"
+beltThreadRemoveTool.order = "c[automated-construction]-d[beltThreadUpgrader-remove-tool]"
+beltThreadRemoveTool.icon = "__BeltThreadUpgrades__/graphics/icons/beltThreadUpgrader-remove-tool-icon-x60.png"
+beltThreadRemoveTool.icon_size = 60
+
+-- Default selection (Remove only connected belts of same tier)
+beltThreadRemoveTool.select.cursor_box_type = "entity"
+beltThreadRemoveTool.select.mode = {"deconstruct"}
+beltThreadRemoveTool.select.entity_filter_mode = "whitelist"
+beltThreadRemoveTool.select.entity_type_filters = selectionTypeFilter
+beltThreadRemoveTool.select.border_color = {0, 0.8, 0}
+
+-- Alternate selection (Remove all belts connected to starting entity)
+beltThreadRemoveTool.alt_select.cursor_box_type = "entity"
+beltThreadRemoveTool.alt_select.mode = "deconstruct"
+beltThreadRemoveTool.alt_select.entity_filter_mode = "whitelist"
+beltThreadRemoveTool.select.entity_type_filters = selectionTypeFilter
+beltThreadRemoveTool.alt_select.border_color = {0, 0.8, 0.8}
+
 -- Add to game
 data:extend{beltThreadUpgradeTool, data.raw["shortcut"]["beltThreadUpgrader-give-belt-upgrade-tool-shortcut"]}
+data:extend{beltThreadRemoveTool, data.raw["shortcut"]["beltThreadUpgrader-give-belt-remove-tool-shortcut"]}
--- prototypes/shortcuts.lua
+++ prototypes/shortcuts.lua
@@ -5,12 +5,29 @@ data:extend(
     name = "beltThreadUpgrader-give-belt-upgrade-tool-shortcut",
     action = "spawn-item",
     associated_control_input = "beltThreadUpgrader-give-belt-upgrade-tool",
-    item_to_spawn = "beltThreadUpgrader-selection-tool",
+    item_to_spawn = "beltThreadUpgrader-upgrade-tool",
     style = "green",
     technology_to_unlock = nil,
-    icon = "__BeltThreadUpgrades__/graphics/icons/beltThreadUpgrader-shortcut-x60.png",
+    icon = "__BeltThreadUpgrades__/graphics/icons/beltThreadUpgrader-upgrade-tool-icon-x60.png",
     icon_size = 60,
-    small_icon = "__BeltThreadUpgrades__/graphics/icons/beltThreadUpgrader-shortcut-x60.png",
+    small_icon = "__BeltThreadUpgrades__/graphics/icons/beltThreadUpgrader-upgrade-tool-icon-x60.png",
+    small_icon_size = 60
+  }
+})
+
+data:extend(
+{
+  {
+    type = "shortcut",
+    name = "beltThreadUpgrader-give-belt-remove-tool-shortcut",
+    action = "spawn-item",
+    associated_control_input = "beltThreadUpgrader-give-belt-remove-tool",
+    item_to_spawn = "beltThreadUpgrader-remove-tool",
+    style = "red",
+    technology_to_unlock = nil,
+    icon = "__BeltThreadUpgrades__/graphics/icons/beltThreadUpgrader-remove-tool-icon-x60.png",
+    icon_size = 60,
+    small_icon = "__BeltThreadUpgrades__/graphics/icons/beltThreadUpgrader-remove-tool-icon-x60.png",
     small_icon_size = 60
   }
 })
